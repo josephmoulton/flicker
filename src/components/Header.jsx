a785efe38016fdd2f2628ef118a70c36621fb85f
Joe <josephlewismoulton@gmail.com>
2022-12-07 17:18:21 +0000

    Header Completed and colour pallete chosen

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
+import "./Header.css";
+import MovieIcon from "@mui/icons-material/Movie";
 
 function Header() {
   return (
-    <div>Header</div>
-  )
+    <div className="header">
+      <div className="container">
+        <div className="logo__container">
+          <h1>Flicker</h1>
+          <div className="logo__icon">
+            <MovieIcon />
+          </div>
+        </div>
+        <div className="header__links">
+          <a className="header__link" href="#">Home</a>
+          <a className="header__link" href="#">Find your movie</a>
+          <a href="/contact"><button className="header__link header__link--button">Contact</button></a>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Header
+export default Header;
