4c5ad34bcadb3f054792f97772a72c2c8725e0b4
Joe <josephlewismoulton@gmail.com>
2022-12-15 16:41:27 +0000

    Movie background and poster generated through api

--- src/pages/MovieProfile.jsx
+++ src/pages/MovieProfile.jsx
@@ -4,11 +4,15 @@ import { useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
 import axios from "axios";
 import placeholderImage from "../assets/DefaultPoster.png";
+import Header from "../components/Header";
 
 function MovieProfile() {
   const { id } = useParams();
+  const [genres, setGenres] = useState([]);
   const [film, setFilm] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [releaseYear, setReleaseYear] = useState("");
+  const [releaseDate, setReleaseDate] = useState("");
 
   async function fetchMovie() {
     setLoading(true);
@@ -16,7 +20,14 @@ function MovieProfile() {
       `https://api.themoviedb.org/3/movie/458156?api_key=72df3e39ae70f2a87b61200cd97ee96b`
     );
     setFilm(data);
+    setGenres(data.genres);
     setLoading(false);
+  }
+
+  function getReleaseYear() {
+    const result = releaseDate.split("-");
+    const [first, second, third] = result;
+    setReleaseYear(first);
   }
 
   useEffect(() => {
@@ -25,6 +36,7 @@ function MovieProfile() {
 
   return (
     <>
+      <Header></Header>
       <div className="film__header">
         <img
           src={
@@ -35,11 +47,29 @@ function MovieProfile() {
           alt="film backdrop"
           className="film__backdrop"
         />
-        <div className="film__title--container">
-          <h1 className="film__title"> {film.title} </h1>
-        </div>
-        <div className="film__tagline--container">
-          <h4 className="film__tagline">{film.tagline}</h4>
+        <div className="film__header--container">
+          <div className="film__data--container">
+            <div className="film__poster--container">
+              <img
+                src={
+                  film.poster_path
+                    ? `https://image.tmdb.org/t/p/w500${film.poster_path}`
+                    : placeholderImage
+                }
+                alt="Film Poster"
+                className="film__poster"
+              />
+              <div className="film__availability">Film Available</div>
+            </div>
+            <div className="film__info--container">
+              <div className="film__title--conatiner">
+                <h1 className="film__title">
+                  {film.title}{" "}
+                  <span className="film__year">({film?.release_date?.split("-")[0]})</span>
+                </h1>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </>
