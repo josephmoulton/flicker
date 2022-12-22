f06507a2c906e1425d5e1fbaf980aca42a269420
Joe <josephlewismoulton@gmail.com>
2022-12-22 15:58:03 +0000

    Scroll to top feature implement --- React router v6

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useLocation,
+} from "react-router-dom";
+import { useLayoutEffect } from "react";
 import Home from "./pages/Home";
 import Testing from "./pages/Testing";
 import MovieProfile from "./pages/MovieProfile";
 import Results from "./pages/Results";
 import Credits from "./pages/Credits";
 
-
 function App() {
   return (
-    <Router forceRefresh={true}>
       <div className="App">
         <Routes>
           <Route path="/" element={<Home />}></Route>
@@ -19,7 +23,6 @@ function App() {
           <Route path="/profile:id/credits:color" element={<Credits />}></Route>
         </Routes>
       </div>
-    </Router>
   );
 }
 
