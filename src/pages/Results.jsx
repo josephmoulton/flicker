ef0c04b21f2b4a8519105d53e8c2ecad04f0eb16
Joe <josephlewismoulton@gmail.com>
2022-12-22 15:40:55 +0000

    Pagination completed

--- src/pages/Results.jsx
+++ src/pages/Results.jsx
@@ -5,9 +5,11 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import { Navigate, useNavigate } from "react-router-dom";
 import Header from "../components/Header";
+import Footer from "../components/Footer";
 import placeholderImage from "../assets/DefaultPoster.png";
 import MovieCard from "../components/MovieCard";
 import SearchBox from "../components/SearchBox";
+import { Pagination } from "@mui/material";
 
 function Results() {
   const { searchResult } = useParams();
@@ -15,10 +17,6 @@ function Results() {
   const [loading, setLoading] = useState(true);
   const [pageNo, setPageNo] = useState(1);
   const [maxPages, setMaxPages] = useState(0);
-
-  function onSearch() {
-    fetchMovies();
-  }
 
   async function fetchMovies() {
     setLoading(true);
@@ -29,16 +27,41 @@ function Results() {
     setMaxPages(data.total_pages);
     setLoading(false);
   }
-
   useEffect(() => {
+    setPageNo(1);
     fetchMovies();
   }, [searchResult]);
 
+  useEffect(() => {
+    fetchMovies();
+  }, [pageNo]);
+
   return (
     <>
-      <Header/>
+      <Header />
       <div className="search__wrapper">
         <SearchBox result={searchResult} />
+      </div>
+      <div className="pagination__container">
+        {maxPages > 10 ? (
+          <Pagination
+            count={maxPages}
+            size="large"
+            variant="outlined"
+            showFirstButton
+            showLastButton
+            page={pageNo}
+            onChange={(e, value) => setPageNo(value)}
+          />
+        ) : (
+          <Pagination
+            count={maxPages}
+            size="large"
+            variant="outlined"
+            page={pageNo}
+            onChange={(e, value) => setPageNo(value)}
+          />
+        )}
       </div>
       <div className="container">
         <div className="row">
@@ -56,6 +79,7 @@ function Results() {
           </div>
         </div>
       </div>
+      <Footer bg={`#e76f51`} />
     </>
   );
 }
